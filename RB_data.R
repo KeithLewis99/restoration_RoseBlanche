458f7201aa28e824676fa96855a797c64207e281
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2024-12-01 21:54:14 -0330

    Minor: add project set up text

--- RB_data.R
+++ RB_data.R
@@ -1,6 +1,27 @@
 # This is just my attempt to consolidate Kristin Loughlin's code in a way that I understand it.  This file is for importing data and manipulating it for the various graphs.  I can now create all or almost all of Kristin's plots for Rose Blanche.
 
+# Set up a project - see the below link for directions.
+#https://happygitwithr.com/rstudio-git-github.html
+
+# But basically:
+# 1.	Set up a Git repo on GitHub.
+# 2.	Create the project in R - New Project - Version Control - Git
+# 3. type "git add -A" in the terminal
+# 4.	Create a bunch of directories automatically (see below)
+# 5. Copy git -ignore file
+
+
 ## Rose Blance
+#Create a "name_dat.R" file
+#put this file in the folder with the project and create the following subfolders
+if(!dir.exists("archive"))dir.create("archive")
+if(!dir.exists("data"))dir.create("data")
+if(!dir.exists("data_derived"))dir.create("data_derived")
+if(!dir.exists("figs"))dir.create("figs") #for publication quality only
+if(!dir.exists("output"))dir.create("output") # for tables and figures
+if(!dir.exists("ms"))dir.create("ms") # manuscript
+if(!dir.exists("report"))dir.create("report") #for rmd report
+if(!dir.exists("refs"))dir.create("refs") #for rmd report
 
 # libraries ----
 library(plyr)
